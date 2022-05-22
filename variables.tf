3f91acdede7d3269ba7acf4307831798f012a552
Nazareno Anselmi <eanselmi@edrans.com>
2022-05-22 17:56:34 -0300

    Use variables to specify region in VPC

--- variables.tf
+++ variables.tf
@@ -10,18 +10,22 @@ variable "ssh_port" {
   default     = 22
 }
 
-variable "vpc_cidr" {  
-  default     = "10.200.0.0/16"
+variable "vpc_cidr" {
+  default = "10.200.0.0/16"
 }
 
-variable "public_subnets"{
-  default = ["10.200.0.0/24","10.200.1.0/24"]
+variable "public_subnets" {
+  default = ["10.200.0.0/24", "10.200.1.0/24"]
 }
 
-variable "private_subnets"{
-  default = ["10.200.2.0/24","10.200.3.0/24"]
+variable "private_subnets" {
+  default = ["10.200.2.0/24", "10.200.3.0/24"]
 }
 
 variable "azs" {
-  default = ["us-east-1a","us-east-1b"]
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "region" {
+  default = "us-east-1"
 }
