020cf719a9b478c2ddf712dc83fc62226986e79b
Nazareno Anselmi <eanselmi@edrans.com>
2022-07-22 15:19:57 -0300

    Vars: add tfvars file and locals

---
+++ local.auto.tfvars
@@ -0,0 +1,7 @@
+server_port     = 80
+ssh_port        = 22
+vpc_cidr        = "10.200.0.0/16"
+public_subnets  = ["10.200.0.0/24", "10.200.1.0/24"]
+private_subnets = ["10.200.2.0/24", "10.200.3.0/24"]
+azs             = ["us-east-1a", "us-east-1b"]
+region          = "us-east-1"
--- variables.tf
+++ variables.tf
@@ -1,31 +1,34 @@
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
-  default     = 80
 }
 
 variable "ssh_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
-  default     = 22
 }
 
 variable "vpc_cidr" {
-  default = "10.200.0.0/16"
+  description = "CIDR for VPC"
+  type        = string
 }
 
 variable "public_subnets" {
-  default = ["10.200.0.0/24", "10.200.1.0/24"]
+  description = "CIDR list for public subnets"
+  type        = list(string)
 }
 
 variable "private_subnets" {
-  default = ["10.200.2.0/24", "10.200.3.0/24"]
+  description = "CIDR list for private subnets"
+  type        = list(string)
 }
 
 variable "azs" {
-  default = ["us-east-1a", "us-east-1b"]
+  description = "List of AZs"
+  type        = list(string)
 }
 
 variable "region" {
-  default = "us-east-1"
+  description = "Region to deploy resources"
+  type        = string
 }
